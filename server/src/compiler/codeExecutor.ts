6a126a443e00709762980a89de6f43d125061f7c
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2023-04-01 13:01:03 -0500

    Add exception handling

--- server/src/compiler/codeExecutor.ts
+++ server/src/compiler/codeExecutor.ts
@@ -9,26 +9,27 @@ interface CodeExecutionResponse {
 
 class CodeExecutor {
   getDockerCommand(language: string, source: string): string[] {
-    const uniqueCppSource = generateUniqueFilename('source', 'cpp');
-    const uniqueCppOutput = generateUniqueFilename('output', 'out');
-    const uniqueJavaFile = generateUniqueFilename('Main', 'java');
+    const escapedSource = source.replace(/"/g, '\\"');
+    const cppFile = generateUniqueFilename('source', 'cpp');
+    const cppOutput = generateUniqueFilename('output', 'out');
+    const javaFile = generateUniqueFilename('Main', 'java');
 
     const languageToCommand: { [language: string]: string[] } = {
-      python: ['python', '-c', source],
+      python: ['python', '-c', `"${escapedSource}"`],
       cpp: [
         'bash',
         '-c',
-        `echo "${source.replace(/"/g, '\\"')}" > ${uniqueCppSource}`,
-        `&& g++ -o ${uniqueCppOutput} ${uniqueCppSource}`,
-        `&& ./${uniqueCppOutput}`,
-        `&& rm ${uniqueCppSource} && rm ${uniqueCppOutput}`,
+        `echo "${escapedSource}" > ${cppFile}`,
+        `&& g++ -o ${cppOutput} ${cppFile}`,
+        `&& ./${cppOutput}`,
+        `&& rm ${cppFile} && rm ${cppOutput}`,
       ],
       java: [
         'bash',
         '-c',
-        `echo "${source.replace(/"/g, '\\"')}" > ${uniqueJavaFile}`,
-        `&& javac ${uniqueJavaFile} && java -cp . ${uniqueJavaFile.replace('.java', '')}`,
-        `&& rm ${uniqueJavaFile} && rm ${uniqueJavaFile.replace('.java', '.class')}`,
+        `echo "${escapedSource}" > ${javaFile}`,
+        `&& javac ${javaFile} && java -cp . ${javaFile.replace('.java', '')}`,
+        `&& rm ${javaFile} && rm ${javaFile.replace('.java', '.class')}`,
       ],
     };
 
@@ -36,29 +37,43 @@ class CodeExecutor {
   }
 
   async runDocker(language: string, source: string): Promise<[string, string]> {
-    const imageName = CompilerConfig.getDockerImage(language);
-    if (!imageName) throw new Error('Unsupported language');
+    try {
+      const imageName = CompilerConfig.getDockerImage(language);
+      if (!imageName) throw new Error('Unsupported language');
 
-    const command = this.getDockerCommand(language, source);
-    const dockerCommand = ['docker', 'run', '--rm', imageName, ...command].join(' ');
+      const command = this.getDockerCommand(language, source);
+      const dockerCommand = ['docker', 'run', '--network', 'none', '--rm', imageName, ...command].join(' ');
 
-    return new Promise((resolve, reject) => {
-      exec(dockerCommand, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve([stdout, stderr]);
-        }
+      return new Promise((resolve, reject) => {
+        exec(dockerCommand, (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve([stdout, stderr]);
+          }
+        });
       });
-    });
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Docker execution failed: ${error.message}`);
+      } else {
+        throw new Error(`Docker execution failed: ${error}`);
+      }
+    }
   }
 
   async execute(language: string, source: string): Promise<CodeExecutionResponse> {
-    const [stdout, stderr] = await this.runDocker(language, source);
-
-    return {
-      output: stdout,
-      error: stderr
+    try {
+      const [stdout, stderr] = await this.runDocker(language, source);
+      return {
+        output: stdout,
+        error: stderr
+      }
+    } catch (error) {
+      return {
+        output: '',
+        error: error instanceof Error ? error.message : String(error)
+      };
     }
   }
 }
